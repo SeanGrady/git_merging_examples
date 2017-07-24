4c7fa41fe333f05167dbe07a756262e304a514e4
Sean Grady <vedicmonk@gmail.com>
2017-07-24 14:34:34 -0700

    first commit

---
+++ web_page.py
@@ -0,0 +1,29 @@
+import things
+
+
+class Page(object):
+    def __init__(self):
+        pass
+
+
+class Button(object):
+    def __init__(self, size, color, link):
+        self.size = size
+        self.color = color
+        self.link = link
+
+mypage = Page()
+
+login_button = Button(
+    size = 5,
+    color = "blue",
+    link = login_link,
+)
+
+logout_button = Button(
+    size = 7,
+    color = "red",
+    link = logout_link,
+)
+
+mypage.buttons.add(login_button, logout_button):
